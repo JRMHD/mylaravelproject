08cbe80465aab40755937eb330971e78e9a36a03
JRMHD <117481963+JRMHD@users.noreply.github.com>
2023-09-27 00:14:50 +0300

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/home', [HomeController::class, 'index']);
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
